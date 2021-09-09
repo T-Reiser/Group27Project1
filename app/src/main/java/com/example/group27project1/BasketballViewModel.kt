87e644739e201e0ebc4ee0fe71bb5854c682788c
T-Reiser <treiser000@gmail.copm>
2021-09-09 18:30:00 -0400

    Fixed Landscape view to prevent crashing, Added ViewModel class to preserve data on rotation and persist when memory is low, added extra button to remove 1 for each team.

---
+++ app/src/main/java/com/example/group27project1/BasketballViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.group27project1
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+private const val TAG = "BasketballViewModel"
+
+class BasketballViewModel : ViewModel(){
+
+    init {
+        Log.d(TAG, "ViewModel instance created")
+    }
+    override fun onCleared() {
+        super.onCleared()
+        Log.d(TAG, "ViewModel instance about to be destroyed")
+    }
+    //store team scores here
+    var aScore: Int = 0
+    var bScore: Int = 0
+
+    //add methods here
+    val getCurrentAScore: Int
+        get() = aScore
+    val getCurrentBScore: Int
+        get() = bScore
+
+    fun setCurrentAScore(newScore: Int) {
+        aScore = newScore
+    }
+
+    fun setCurrentBScore(newScore: Int) {
+        bScore = newScore
+    }
+}
--- app/src/main/java/com/example/group27project1/MainActivity.kt
+++ app/src/main/java/com/example/group27project1/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.group27project1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+
+private const val TAG = "MainActivity"
+private const val aKEY_INDEX = "aindex"
+private const val bKEY_INDEX = "bindex"
 
 class MainActivity : AppCompatActivity() {
-    private var aScore: Int = 0
-    private var bScore: Int = 0
+
     private lateinit var teamAScoreTextView: TextView
     private lateinit var teamBScoreTextView: TextView
     private lateinit var freeABtn: Button
@@ -18,9 +24,27 @@ class MainActivity : AppCompatActivity() {
     private lateinit var threeBBtn: Button
     private lateinit var resetBtn: Button
 
+    private lateinit var  rem1ABtn: Button
+    private lateinit var rem1BBtn: Button
+
+    private val bbViewModel: BasketballViewModel by lazy {
+        ViewModelProviders.of(this).get(BasketballViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val acurrentIndex = savedInstanceState?.getInt(aKEY_INDEX, 0) ?: 0
+        bbViewModel.aScore = acurrentIndex
+        val bcurrentIndex = savedInstanceState?.getInt(bKEY_INDEX, 0) ?: 0
+        bbViewModel.bScore = bcurrentIndex
+
+
+        //val provider: ViewModelProvider = ViewModelProviders.of(this)
+        //val bbViewModel = provider.get(BasketballViewModel::class.java)
+        Log.d("BasketballViewModel", "Got a BasketballViewModel: $bbViewModel")
+
 
         freeABtn = findViewById(R.id.freeA_btn)
         freeBBtn = findViewById(R.id.freeB_btn)
@@ -28,45 +52,99 @@ class MainActivity : AppCompatActivity() {
         twoBBtn = findViewById(R.id.twoB_btn)
         threeABtn = findViewById(R.id.threeA_btn)
         threeBBtn = findViewById(R.id.threeB_btn)
+
+        rem1ABtn = findViewById(R.id.undoA_btn)
+        rem1BBtn = findViewById(R.id.undoB_btn)
+
         teamAScoreTextView = findViewById(R.id.teamA_score)
         teamBScoreTextView = findViewById(R.id.teamB_score)
         resetBtn = findViewById(R.id.reset_btn)
 
         freeABtn.setOnClickListener {
-            aScore++
-            teamAScoreTextView.text = aScore.toString()
+            bbViewModel.setCurrentAScore(bbViewModel.getCurrentAScore +1)
+            //teamAScoreTextView.text = bbViewModel.getCurrentAScore.toString()
+            updateScores()
         }
 
         freeBBtn.setOnClickListener {
-            bScore++
-            teamBScoreTextView.text = bScore.toString()
+            bbViewModel.setCurrentBScore(bbViewModel.getCurrentBScore +1)
+            //teamBScoreTextView.text = bbViewModel.getCurrentBScore.toString()
+            updateScores()
+
         }
 
         twoABtn.setOnClickListener {
-            aScore+=2
-            teamAScoreTextView.text = aScore.toString()
+            bbViewModel.setCurrentAScore(bbViewModel.getCurrentAScore +2)
+            updateScores()
         }
 
         twoBBtn.setOnClickListener {
-            bScore+=2
-            teamBScoreTextView.text = bScore.toString()
+            bbViewModel.setCurrentBScore(bbViewModel.getCurrentBScore +2)
+            updateScores()
         }
 
         threeABtn.setOnClickListener {
-            aScore+=3
-            teamAScoreTextView.text = aScore.toString()
+            bbViewModel.setCurrentAScore(bbViewModel.getCurrentAScore +3)
+            updateScores()
         }
 
         threeBBtn.setOnClickListener {
-            bScore+=3
-            teamBScoreTextView.text = bScore.toString()
+            bbViewModel.setCurrentBScore(bbViewModel.getCurrentBScore +3)
+            updateScores()
+        }
+
+        rem1ABtn.setOnClickListener {
+            bbViewModel.setCurrentAScore(bbViewModel.getCurrentAScore -1)
+            updateScores()
+        }
+        rem1BBtn.setOnClickListener {
+            bbViewModel.setCurrentBScore(bbViewModel.getCurrentBScore -1)
+            updateScores()
         }
 
         resetBtn.setOnClickListener {
-            aScore = 0
-            bScore = 0
-            teamAScoreTextView.text = aScore.toString()
-            teamBScoreTextView.text = bScore.toString()
+            bbViewModel.setCurrentAScore(0)
+            bbViewModel.setCurrentBScore(0)
+            updateScores()
         }
+        updateScores()
     }
+
+    private fun updateScores() {
+        teamAScoreTextView.setText(bbViewModel.getCurrentAScore.toString())
+        teamBScoreTextView.setText(bbViewModel.getCurrentBScore.toString())
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        Log.i(TAG, "onSaveInstanceState")
+        savedInstanceState.putInt(aKEY_INDEX, bbViewModel.getCurrentAScore)
+        savedInstanceState.putInt(bKEY_INDEX, bbViewModel.getCurrentBScore)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
 }
