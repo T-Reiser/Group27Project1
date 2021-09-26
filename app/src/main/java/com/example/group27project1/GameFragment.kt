7264480cc9fe69b10797c869d3d0592867630dad
T-Reiser <treiser000@gmail.copm>
2021-09-26 16:33:39 -0400

    trying different build configs

--- app/src/main/java/com/example/group27project1/GameFragment.kt
+++ app/src/main/java/com/example/group27project1/GameFragment.kt
@@ -1,10 +1,14 @@
 package com.example.group27project1
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
@@ -14,12 +18,10 @@ import androidx.lifecycle.Observer
 private const val ARG_GAME_ID = "game_id"
 private const val TAG = "GameFragment"
 
-
 class GameFragment : Fragment() {
 
     private lateinit var game: Game
     private lateinit var titleField: EditText
-
 
     private val gameDetailViewModel: GameDetailViewModel by lazy {
         ViewModelProviders.of(this).get(GameDetailViewModel::class.java)
@@ -59,6 +61,41 @@ class GameFragment : Fragment() {
     }
 
 
+    override fun onStart() {
+        super.onStart()
+
+        val titleWatcher = object : TextWatcher {
+
+            override fun beforeTextChanged(
+                sequence: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+                // This space intentionally left blank
+            }
+
+            override fun onTextChanged(
+                sequence: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                //game.id = sequence.toString()
+            }
+
+            override fun afterTextChanged(sequence: Editable?) {
+                // This one too
+            }
+        }
+        titleField.addTextChangedListener(titleWatcher)
+
+//        solvedCheckBox.apply {
+//            setOnCheckedChangeListener { _, isChecked ->
+//                crime.isSolved = isChecked
+//            }
+//        }
+    }
 
     override fun onStop() {
         super.onStop()
@@ -81,7 +118,14 @@ class GameFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.list_item_game, container, false)
 
-        //titleField = view.findViewById(R.id.game_title) as EditText
+//        titleField = view.findViewById(R.id.crime_title) as EditText
+//        dateButton = view.findViewById(R.id.crime_date) as Button
+//        solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
+//
+//        dateButton.apply {
+//            text = crime.date.toString()
+//            isEnabled = false
+//        }
 
         return view
     }
